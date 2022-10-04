df1057a80d5676d20cdf933460825d40d9cdd3d8
Matthew Whitaker <matthew.w3000@gmail.com>
2022-10-04 14:02:38 -0600

    Run formatter

--- packages/list_counter/example/lib/main.dart
+++ packages/list_counter/example/lib/main.dart
@@ -6,25 +6,28 @@ void main() {
   final counter = new Counter('binary-counter');
 
   final counterStyle = CounterStyle.define(
-    name: 'binary',
-    system: System.numeric,
-    padLength: 8,
-    padCharacter: '0',
-    symbols: ['0','1'],
-    range: IntRange(min: -255, max: 255), //Limiting to 255 isn't necessary (it could go on infinitely), but is given as an example
-    fallback: 'decimal' //This is the default. Given as an example
-  );
+      name: 'binary',
+      system: System.numeric,
+      padLength: 8,
+      padCharacter: '0',
+      symbols: ['0', '1'],
+      range: IntRange(
+          min: -255,
+          max:
+              255), //Limiting to 255 isn't necessary (it could go on infinitely), but is given as an example
+      fallback: 'decimal' //This is the default. Given as an example
+      );
 
   print("Binary numbers 1 through 255");
   //Increment through the entire range
-  for(int i = 0; i < 255; i++) {
+  for (int i = 0; i < 255; i++) {
     counter.increment();
     print(counterStyle.generateMarkerContent(counter.value));
   }
 
   print("A few out of range values:");
   //Increment out of the range a few times to show that it reverts to decimal
-  for(int i = 0; i < 5; i++) {
+  for (int i = 0; i < 5; i++) {
     counter.increment();
     print(counterStyle.generateMarkerContent(counter.value));
   }
@@ -34,7 +37,7 @@ void main() {
 
   //Now do it in reverse! Note that padLength includes the negative symbol.
   print("Negative values from -1 to -8");
-  for(int i = 0; i > -8; i--) {
+  for (int i = 0; i > -8; i--) {
     counter.increment(-1);
     print(counterStyle.generateMarkerContent(counter.value));
   }
